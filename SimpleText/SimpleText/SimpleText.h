4bf3d48a81d3b985e49443b469e376fccee51cc1
Steve Dunstan <steve_dunstan@yahoo.com>
2014-07-07 19:50:29 -0700

    Initial Commit

---
+++ SimpleText/SimpleText/SimpleText.h
@@ -0,0 +1,13 @@
+//
+//  SimpleText.h
+//  SimpleText
+//
+//  Created by Stephen Dunstan on 7/7/14.
+//  Copyright (c) 2014 Stephen Dunstan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimpleText : NSObject
+
+@end
---
+++ SimpleText/SimpleText/SimpleText.m
@@ -0,0 +1,13 @@
+//
+//  SimpleText.m
+//  SimpleText
+//
+//  Created by Stephen Dunstan on 7/7/14.
+//  Copyright (c) 2014 Stephen Dunstan. All rights reserved.
+//
+
+#import "SimpleText.h"
+
+@implementation SimpleText
+
+@end
